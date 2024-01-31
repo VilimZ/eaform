3536a4762eb837670c872807d9aa5136727645b4
Vilim Zovko <vilim.zovko@gmail.com>
2024-01-31 13:52:28 +0100

    Form-control css roulse

--- themes/ea-hugo/assets/js/eu_js_form.js
+++ themes/ea-hugo/assets/js/eu_js_form.js
@@ -2,10 +2,7 @@
 *
 *
 */
-const username = 'janedoe';
-const password = 'Mrkva123';
-const username2 = 'vilimtelnet';
-const password2 = '12345678';
+
 
 const uriAppointments = 'https://appointments.aspose.com/index.php/api/v1/services';
 const uriLocal =  'http://easyappointments.local/index.php/api/v1/services/';
@@ -48,60 +45,42 @@ const displayOption = async () => {
 displayOption();
 
 
-
-
 /*
-*
+*      Provider
+*/
 
-const batchTrack = document.getElementById("select-service");
-console.log({ batchTrack });
-const getPost = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await response.json();
+const uriProvider = './providers.json';
+
+const provider = document.getElementById("select-provider");
+console.log({ provider });
+const getProvider = async () => {
+  const response = await fetch(uriProvider, {
+    credentials: 'include',
+    method: 'GET',
+    mode: 'no-cors',
+    headers: new Headers ({
+            'Authorization' : 'Basic' + encodedCredentials,
+            'Accept': 'application/json',
+    })
+    
+  });  
+
+ 
+  
+  const data =  response.json();
   return data;
 };
 
-const displayOption = async () => {
-  const options = await getPost();
+const displayOptionProviders = async () => {
+  const options = await getProvider();
   for (option of options) {
-    const newOption = document.createElement("option");
-    newOption.value = option.username;
-    newOption.text = option.name;
-    batchTrack.appendChild(newOption);
+    const provOption = document.createElement("option");
+    provOption.value = option.firstName;
+    provOption.text = option.firstName + ' ' + option.lastName ;
+    provider.appendChild(provOption);
   }
 };
 
-displayOption();;
+displayOptionProviders();
 
 
-*
-*/
-
-
-/* 
-// Select modal
-var mpopup = document.getElementById('mpopupBox');
-
-// Select trigger link
-var mpLink = document.getElementById("mpopupLink");
-
-// Select close action element
-var close = document.getElementsByClassName("close")[0];
-
-// Open modal once the link is clicked
-mpLink.onclick = function() {
-    mpopup.style.display = "block";
-};
-
-// Close modal once close element is clicked
-close.onclick = function() {
-    mpopup.style.display = "none";
-};
-
-// Close modal when user clicks outside of the modal box
-window.onclick = function(event) {
-    if (event.target == mpopup) {
-        mpopup.style.display = "none";
-    }
-};
- */
